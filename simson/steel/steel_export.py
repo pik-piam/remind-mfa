f26e72ec53bc4fc3b4c9c49cc1167eb3e7203b09
Merjo <m.hosak@pilos.de>
2025-02-03 16:04:09 +0000

    add global per capita visualisation for stocks

--- simson/steel/steel_export.py
+++ simson/steel/steel_export.py
@@ -5,7 +5,6 @@ from simson.common.custom_export import CustomDataExporter
 
 
 class SteelDataExporter(CustomDataExporter):
-
     scrap_demand_supply: dict = {"do_visualize": True}
     sector_splits: dict = {"do_visualize": True}
 
@@ -102,7 +101,7 @@ class SteelDataExporter(CustomDataExporter):
         )
 
     def visualize_regional_stock(
-        self, stock, x_array, population, x_label, y_label, title, per_capita, over_gdp
+            self, stock, x_array, population, x_label, y_label, title, per_capita, over_gdp
     ):
         if per_capita:
             stock = stock / population
@@ -126,7 +125,7 @@ class SteelDataExporter(CustomDataExporter):
         self.plot_and_save_figure(ap_stock, "stocks_by_region.png")
 
     def visiualize_global_stock(
-        self, stock, x_array, population, x_label, y_label, title, per_capita, over_gdp
+            self, stock, x_array, population, x_label, y_label, title, per_capita, over_gdp
     ):
         if over_gdp:
             x_array = x_array * population
@@ -138,10 +137,10 @@ class SteelDataExporter(CustomDataExporter):
         self.visualize_global_stock_by_good(
             stock, x_array, population, x_label, y_label, title, per_capita
         )
-        self.visualize_global_stock_by_region(stock, x_array, x_label, y_label, title, per_capita)
+        self.visualize_global_stock_by_region(stock, x_array, population, x_label, y_label, title, per_capita)
 
     def visualize_global_stock_by_good(
-        self, stock, x_array, population, x_label, y_label, title, per_capita
+            self, stock, x_array, population, x_label, y_label, title, per_capita
     ):
         stock = stock.sum_over("r")
         stock = stock / population.sum_over("r") if per_capita else stock
@@ -160,12 +159,11 @@ class SteelDataExporter(CustomDataExporter):
 
         self.plot_and_save_figure(ap_stock, "stocks_global_by_good.png")
 
-    def visualize_global_stock_by_region(self, stock, x_array, x_label, y_label, title, per_capita):
-        if per_capita:
-            logging.info("Global stocks by region can not be implemented per capita. Skipping...")
-            return
-
+    def visualize_global_stock_by_region(self, stock, x_array, population, x_label, y_label, title, per_capita):
         stock = stock.sum_over("g")
+
+        if per_capita:
+            stock = stock / population
 
         ap_stock = self.plotter_class(
             array=stock,
@@ -175,25 +173,25 @@ class SteelDataExporter(CustomDataExporter):
             x_array=x_array,
             xlabel=x_label,
             ylabel=y_label,
-            title=f"{title} (global by region)",
-            area=True,
+            title=f"{title} (global by region{' per capita' if per_capita else ''})",
+            area=False if per_capita else True,
         )
 
-        self.plot_and_save_figure(ap_stock, "stocks_global_by_region.png")
+        self.plot_and_save_figure(ap_stock, f"stocks_global_by_region{'_per_capita' if per_capita else ''}.png")
 
     def visualize_scrap_demand_supply(self, mfa: fd.MFASystem):
         flw = mfa.flows
         prm = mfa.parameters
 
         total_production = (
-            flw["forming => ip_market"] / prm["forming_yield"] / prm["production_yield"]
+                flw["forming => ip_market"] / prm["forming_yield"] / prm["production_yield"]
         )
         dri = prm["dri_production"] + prm["dri_imports"] - prm["dri_exports"]
         pigiron = (
-            prm["pigiron_production"]
-            + prm["pigiron_imports"]
-            - prm["pigiron_exports"]
-            - prm["pigiron_to_cast"]
+                prm["pigiron_production"]
+                + prm["pigiron_imports"]
+                - prm["pigiron_exports"]
+                - prm["pigiron_to_cast"]
         )
 
         total_production = total_production[{"t": mfa.dims["h"]}]
@@ -201,9 +199,9 @@ class SteelDataExporter(CustomDataExporter):
         # scrap is also used in pig iron and dri production
         scrap_demand += prm["pigiron_production"] * 0.15 + prm["dri_production"] * 0.06
         scrap_supply = (
-            flw["recycling => scrap_market"]
-            + flw["forming => scrap_market"]
-            + flw["fabrication => scrap_market"]
+                flw["recycling => scrap_market"]
+                + flw["forming => scrap_market"]
+                + flw["fabrication => scrap_market"]
         )
         scrap_supply = scrap_supply[{"t": mfa.dims["h"]}].sum_over("e")
 
