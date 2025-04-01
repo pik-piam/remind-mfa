af803ac5307f3c69dba1646e26b500a39e6299da
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-04-01 15:18:17 +0200

    refactor: rename check_non_negative_flows to check_flows for consistency

--- simson/steel/steel_mfa_system_historic.py
+++ simson/steel/steel_mfa_system_historic.py
@@ -18,7 +18,7 @@ class InflowDrivenHistoricSteelMFASystem(fd.MFASystem):
         self.compute_flows()
         self.compute_in_use_stock()
         self.check_mass_balance()
-        self.check_non_negative_flows(no_error=True)
+        self.check_flows(no_error=True)
 
     def compute_trade(self):
         """
