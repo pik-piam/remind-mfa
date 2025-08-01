b5e3e7d65d1161e446b6e928b7a4e0b3945fe7c1
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-08-01 19:15:01 +0200

    improve Bound behavior

--- remind_mfa/common/data_transformations.py
+++ remind_mfa/common/data_transformations.py
@@ -1,6 +1,6 @@
 import flodym as fd
 import numpy as np
-from typing import Optional
+from typing import Union
 from pydantic import model_validator, Field
 
 from remind_mfa.common.base_model import RemindMFABaseModel
@@ -19,9 +19,11 @@ class Bound(RemindMFABaseModel):
     Flodym-compatible bounds for a parameter.
     """
 
-    var_name: Optional[str]
+    var_name: str
+    """Name of the variable for which the bounds are defined."""
     dims: fd.DimensionSet = fd.DimensionSet(dim_list=[])
-    """Dimensions of the bounds. Not required if bounds are scalar."""
+    """Dimensions of the bounds. Not required if bounds are scalar.
+    May also be part of the lower and upper bounds."""
     lower_bound: fd.FlodymArray
     upper_bound: fd.FlodymArray
 
@@ -32,11 +34,16 @@ class Bound(RemindMFABaseModel):
         for field in required_fields:
             if field not in data:
                 raise ValueError(f"Missing required field: {field}")
-
+        
+        # extract variables
         var_name = data.get("var_name")
         dims = data.get("dims")
-        lower = np.array(data.get("lower_bound"), dtype=float)
-        upper = np.array(data.get("upper_bound"), dtype=float)
+        lower = data.get("lower_bound")
+        upper = data.get("upper_bound")
+
+        # prepare bounds and dimensions
+        lower, dims = cls._extract_bound_array(lower, dims, "lower")
+        upper, dims = cls._extract_bound_array(upper, dims, "upper")
 
         if dims is None:
             dims = cls.model_fields.get("dims").default
@@ -47,6 +54,22 @@ class Bound(RemindMFABaseModel):
             "upper_bound": fd.FlodymArray(dims=dims, values=upper, name="upper_bound"),
             "dims": dims,
         }
+    
+    @staticmethod
+    def _extract_bound_array(input_val, dims, bound_name):
+        """
+        Extracts the array from the input value, ensuring it matches the expected dimensions.
+        """
+        if isinstance(input_val, np.ndarray):
+            return input_val.copy().astype(np.float64), dims
+        elif isinstance(input_val, fd.FlodymArray):
+            if dims is None:
+                dims = input_val.dims
+            elif input_val.dims != dims:
+                raise ValueError(f"{bound_name.capitalize()} bound dimensions {input_val.dims} do not match expected dims {dims}.")
+            return input_val.values.copy().astype(np.float64), dims
+        else:
+            return np.array(input_val, dtype=np.float64), dims
 
     @model_validator(mode="after")
     def validate_bounds(self):
@@ -56,20 +79,11 @@ class Bound(RemindMFABaseModel):
         if np.any(lb > ub):
             raise ValueError("Lower bounds must be smaller than upper bounds")
 
-        # Check if lower bound equals upper bound
+        # Check if lower bound equals upper bound and slightly adjust (required for scipy optimizer)
         equal_mask = lb == ub
         if np.any(equal_mask):
-            adjustment = 1e-10
-            zero_mask = (lb == 0) & (ub == 0)
-
-            # Handle case where both bounds are 0
-            lb[zero_mask] = -adjustment
-            ub[zero_mask] = adjustment
-
-            # Handle general case where bounds are equal
-            non_zero_mask = equal_mask & np.logical_not(zero_mask)
-            lb[non_zero_mask] -= adjustment * np.abs(lb[non_zero_mask])
-            ub[non_zero_mask] += adjustment * np.abs(ub[non_zero_mask])
+            lb[equal_mask] = np.nextafter(lb[equal_mask], -np.inf) * (1 + 1e-6)
+            ub[equal_mask] = np.nextafter(ub[equal_mask], np.inf) * (1 + 1e-6)
 
         return self
 
