4084ef8982304aed7f8c45d31a0817eb5d410eb2
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2026-05-08 14:06:08 +0200

    add historic data passthrough also in cement model

--- remind_mfa/cement/cement_model.py
+++ remind_mfa/cement/cement_model.py
@@ -74,7 +74,7 @@ class CementModel(CommonModel):
         # apply scenarios to parameters for future mfa (as in common model)
         # 1. extend historic parameters into future
         self.parameters = ParameterExtrapolationManager(
-            self.cfg, self.dims["t"]
+            self.cfg, self.dims["h"], self.dims["t"]
         ).apply_prm_extrapolation(self.parameters, self.scenario_parameters)
         # 2. adjust future parameters based on scenario
         self.apply_scenario_adjustments_to_parameters()
