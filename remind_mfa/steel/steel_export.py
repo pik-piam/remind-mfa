b95a5bcd57820ee7ca5d5e1489b922b64462e653
Tobias Diez <tobias.diez@pik-potsdam.de>
2026-07-29 16:24:30 +0200

    Update steel export
    Co-authored-by: Jakob Duerrwaechter <jakob.duerrwaechter@pik-potsdam.de>

--- remind_mfa/steel/steel_export.py
+++ remind_mfa/steel/steel_export.py
@@ -9,26 +9,44 @@ from remind_mfa.common.common_export import (
 
 class SteelDataExporter(CommonDataExporter):
     @staticmethod
-    def _steel_production(mfa: fd.MFASystem) -> fd.FlodymArray:
-        """Raw steel output after production and forming losses, before trade."""
+    def _total_steel_production(mfa: fd.MFASystem) -> fd.FlodymArray:
+        """Total steel output after production and forming losses, before trade."""
         return mfa.flows["forming => ip_market"].sum_to(("t", "r"))
 
     @staticmethod
-    def _steel_scrap(mfa: fd.MFASystem) -> fd.FlodymArray:
-        """Total scrap available after collection and before the model diverts any surplus to excess scrap."""
+    def _secondary_steel_production(mfa: fd.MFASystem) -> fd.FlodymArray:
+        """Secondary steel output. For the moment, this is steel produced from scrap in the EAF."""
+        return mfa.flows["eaf_production => forming"].sum_to(("t", "r"))
+
+    @staticmethod
+    def _eol_scrap_potential(mfa: fd.MFASystem) -> fd.FlodymArray:
+        """End-of-life scrap available before collection and trade."""
         return mfa.stocks["in_use"].outflow.sum_to(("t", "r", "g"))
 
-    def get_mrindustry_variables(self) -> list[RemindInputVariable]:
+    @staticmethod
+    def _total_available_scrap(mfa: fd.MFASystem) -> fd.FlodymArray:
+        """Home and new scrap, plus old scrap after collection and trade, but before the model diverts any surplus to excess scrap."""
+        return (
+            mfa.flows["forming => scrap_market"]
+            + mfa.flows["fabrication => scrap_market"]
+            + mfa.flows["recycling => scrap_market"]
+        )
 
+    def get_mrindustry_variables(self) -> list[RemindInputVariable]:
         return [
             RemindInputVariable(
-                name="steel_production",
-                calculation_function=SteelDataExporter._steel_production,
+                name="steel_production_total",
+                calculation_function=SteelDataExporter._total_steel_production,
+                unit="t/yr",
+            ),
+            RemindInputVariable(
+                name="steel_production_secondary",
+                calculation_function=SteelDataExporter._secondary_steel_production,
                 unit="t/yr",
             ),
             RemindInputVariable(
                 name="steel_scrap",
-                calculation_function=SteelDataExporter._steel_scrap,
+                calculation_function=SteelDataExporter._total_available_scrap,
                 unit="t/yr",
             ),
         ]
@@ -37,7 +55,7 @@ class SteelDataExporter(CommonDataExporter):
         return [
             IamcVariable(
                 variable_name="Production|Iron and Steel|Steel",  # PRISMA nomenclature
-                calculation_function=SteelDataExporter._steel_production,
+                calculation_function=SteelDataExporter._total_steel_production,
                 unit="t/yr",
             ),
             IamcVariable(
@@ -56,7 +74,7 @@ class SteelDataExporter(CommonDataExporter):
             ),
             IamcVariable(
                 variable_name="Scrap|Iron and Steel|Steel",  # PRISMA nomenclature
-                calculation_function=SteelDataExporter._steel_scrap,
+                calculation_function=SteelDataExporter._eol_scrap_potential,
                 unit="t/yr",
                 split_name="Good",
             ),
