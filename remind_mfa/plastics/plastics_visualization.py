64d1c9bfabf445072dbaa21397e6ecae943e6250
Leonie Schweiger <leonie.schweiger@pik-potsdam.de>
2026-08-04 15:55:31 +0200

    visualize plastics production; remove compare_demand plot (now replaced by validation.mif plotting script)

--- remind_mfa/plastics/plastics_visualization.py
+++ remind_mfa/plastics/plastics_visualization.py
@@ -25,14 +25,16 @@ class PlasticsVisualizer(CommonVisualizer):
                 regional=False,
             )
 
-        if self.cfg.consumption.do_visualize:
-            self.compare_demand(mfa=model.future_mfa)
+        if self.cfg.material_splits.do_visualize:
             self.visualize_material_splits(mfa=model.future_mfa)
+
+        if self.cfg.production.do_visualize:
+            self.visualize_production(mfa=model.future_mfa, regional=True)
+            self.visualize_production(mfa=model.future_mfa, regional=False)
 
         if self.cfg.extrapolation.do_visualize:
             self.visualize_extrapolation(model=model, subplot_dim="Good", linecolor_dim="Region")
             self.visualize_extrapolation(model=model, subplot_dim="Region", linecolor_dim="Good")
-            self.visualize_extrapolation_functions(model=model, stock_handler=model.stock_handler)
 
         if self.cfg.flows.do_visualize:
             self.visualize_fdarr_stacked(
@@ -113,6 +115,10 @@ class PlasticsVisualizer(CommonVisualizer):
             per_capita=per_capita,
             regional=True,
         )
+
+    def visualize_production(self, mfa: fd.MFASystem, regional=True):
+        production = mfa.flows["polymerization => primary_market"] + mfa.flows["reclmech => primary_market"]
+        self.visualize_fdarr(mfa=mfa, flow=production, name="Plastics production", regional=regional)
 
     def compare_demand(self, mfa: fd.MFASystem):
         df = pd.read_csv("data/plastics/input/validation.csv", sep=";")
