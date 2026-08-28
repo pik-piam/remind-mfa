f331e2af1b98b928658c6dd96fac0e3253f9c265
Tobias Diez <tobias.diez@pik-potsdam.de>
2026-08-28 08:52:21 +0200

    fix config loading

--- notebooks/process_graphs.py
+++ notebooks/process_graphs.py
@@ -1,24 +1,28 @@
 import marimo
 
-__generated_with = "0.23.15"
+__generated_with = "0.24.0"
 app = marimo.App()
 
 
 @app.cell
 def _():
+    from remind_mfa.common.helpers import ModelNames
     import dotenv
     import marimo as mo
+    from pathlib import Path
     from flodym.export import GraphvizProcessGraphPlotter
 
-    from run_remind_mfa import init_model, read_model_config
+    from remind_mfa.common.helpers import init_model
+    from remind_mfa.common.config_loader import load_config
 
     dotenv.load_dotenv()
+    CONFIG_PATH = Path(__file__).parents[1] / "config" / "default.toml"
 
     graphs = []
     for historic in [True, False]:
         graphs.append(mo.md(f"# MFA {'Historic' if historic else 'Future'}"))
-        for model_name in ["steel", "cement", "plastics"]:
-            model_config = read_model_config(f"../config/{model_name}.yml")
+        for model_name in ModelNames:
+            model_config = load_config([CONFIG_PATH], model_name)
             model = init_model(cfg=model_config)
             mfa = model.make_mfa(historic=historic)
             dot = GraphvizProcessGraphPlotter(mfa=mfa, rankdir="LR").plot()
