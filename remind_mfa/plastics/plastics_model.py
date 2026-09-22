f06ac0a485b4c795b7d42ea2520732ca303d6909
Jakob Duerrwaechter <44020564+JakobBD@users.noreply.github.com>
2026-09-22 09:42:57 +0200

    Update remind_mfa/plastics/plastics_model.py
    Co-authored-by: Leonie Schweiger <103267387+leonieschweiger@users.noreply.github.com>

--- remind_mfa/plastics/plastics_model.py
+++ remind_mfa/plastics/plastics_model.py
@@ -31,6 +31,7 @@ class PlasticsModel(CommonModel):
 
     do_stock_extrapolation_with_time_factor: bool = True
     time_factor_prms = {"horizontal_shift_base": 1980, "growth_rate": 0.01}
+    # these are the parameters for a Gompertz function that reaches 20% saturation in 1950 and 80% in 2020
 
     def modify_parameters(self):
         # cast lifetime mean to correct dimensions for use in common model
